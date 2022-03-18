d20138b5d32971c8f76be46add60278018ed623e
ridhodaffasyah <ridhodaffasyah01@gmail.com>
2022-03-19 02:01:48 +0700

    add html, css, js for playlist and console log id client

--- spotify-app/src/index.js
+++ spotify-app/src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+document.getElementById("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  const title = document.getElementById("title").value;
+  const desc = document.getElementById("description").value;
+  alert(
+    `Berhasil menambahkan playlist ! \nTitle : ${title} \nDescription : ${desc}`
+  );
+  document.getElementById("title").value = "";
+  document.getElementById("description").value = "";
+});
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//use fetch and not async
+function getAPI() {
+  fetch(
+    "https://gist.githubusercontent.com/aryapradipta9/e6492383477803b233916e01f36d5465/raw/66942c739d66d3774303f84071696aa865a07077/single-sample.json"
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(`Using fetch not async :`, data);
+    })
+    .catch((err) => alert(`Error : ${err.data}`));
+}
+
+//use fetch and async
+async function getAsyncAPI() {
+  try {
+    const respAsync = await fetch(
+      "https://gist.githubusercontent.com/aryapradipta9/e6492383477803b233916e01f36d5465/raw/66942c739d66d3774303f84071696aa865a07077/single-sample.json"
+    );
+    const data = await respAsync.json();
+    console.log(`Using fetch async :`, data);
+  } catch (e) {
+    alert(`Error : ${e}`);
+  }
+}
+
+const secret_key = process.env.REACT_APP_SPOTIFY_ID_CLIENT;
+
+document.getElementById("btn").onclick = function GetAPI() {
+  getAsyncAPI();
+  getAPI();
+  console.log(secret_key);
+};
+
